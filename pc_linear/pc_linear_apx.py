5e2fafac25ce6f75f7742921c072719a4b0e04de
Riya Kanabar <riyamkanabar1608@gmail.com>
2025-05-29 17:26:07 -0700

    fix approximate_pc_linear_fx algorithm

--- pc_linear/pc_linear_apx.py
+++ pc_linear/pc_linear_apx.py
@@ -36,8 +36,8 @@ def find_intersection(line1_start, line1_end, line2_start, line2_end):
         tuple: x and y coordinates of the intersection, or None if lines are parallel
     """
     denominator = (line1_end[0] - line1_start[0]) * (line2_end[1] - line2_start[1]) - (
-                line2_end[0] - line2_start[0]) * (line1_end[1] - line1_start[1])
-    if abs(denominator) < 1e-10:  # Handle nearly parallel lines
+            line2_end[0] - line2_start[0]) * (line1_end[1] - line1_start[1])
+    if abs(denominator) < 1e-10:
         return None
     x = ((line1_end[0] * line1_start[1] - line1_start[0] * line1_end[1]) * (line2_end[0] - line2_start[0]) -
          (line2_end[0] * line2_start[1] - line2_start[0] * line2_end[1]) * (
@@ -46,218 +46,106 @@ def find_intersection(line1_start, line1_end, line2_start, line2_end):
          (line2_end[0] * line2_start[1] - line2_start[0] * line2_end[1]) * (
                      line1_end[1] - line1_start[1])) / denominator
     return (np.round(x, 6), np.round(y, 6))
-def approximate_pc_linear_fx(pc_linear_fx, epsilon):
-    """
-    Constructs optimal polygon and returns pivot points.
-    Based on 'An Optimal Algorithm for Approximating a Piecewise Linear
-    Function' by HIROSHI IMAI and MASAO IRI.
+def approximate_pc_linear_fx(pc_linear_fx, w):
+    y = reconstruct_piecewise_function(pc_linear_fx)
+    y = np.array(y)
+    x = np.arange(len(y))
+    y = np.round(y, 6)
+    p_plus = (x[0], y[0] + w)
+    l_plus = (x[0], y[0] + w)
+    r_plus = (x[1], y[1] + w)
+    s_plus = {(x[0], y[0] + w): (x[1], y[1] + w)}
+    t_plus = {(x[1], y[1] + w): (x[0], y[0] + w)}
+    p_minus = (x[0], y[0] - w)
+    l_minus = (x[0], y[0] - w)
+    r_minus = (x[1], y[1] - w)
+    s_minus = {(x[0], y[0] - w): (x[1], y[1] - w)}
+    t_minus = {(x[1], y[1] - w): (x[0], y[0] - w)}
+    q = []
+    i = 2
 
-    Args:
-        y_values (np.array or list): Time series or y-values of the function to approximate
-        epsilon (float, optional): Error bound
+    while i < len(y):
+        p = (x[i - 1], y[i - 1] + w)
+        p_i_plus = (x[i], y[i] + w)
+        while (p != p_plus) and calculate_angle(p_i_plus, p, t_plus[p], '+') > np.pi:
+            p = t_plus[p]
+        s_plus[p] = p_i_plus
+        t_plus[p_i_plus] = p
 
-    Returns:
-        np.array: Pivot points of the optimal approximation
-    """
-    # Convert input to numpy array and create x coordinates
-    y_values = reconstruct_piecewise_function(pc_linear_fx)
-    y_values = np.array(y_values)
-    x_values = np.arange(len(y_values))
+        p = (x[i - 1], y[i - 1] - w)
+        p_i_minus = (x[i], y[i] - w)
+        while (p != p_minus) and calculate_angle(p_i_minus, p, t_minus[p], '-') > np.pi:
+            p = t_minus[p]
+        s_minus[p] = p_i_minus
+        t_minus[p_i_minus] = p
 
-    # Initialization
-    y_values = np.round(y_values, 6)
-
-    # Upper tunnel boundary points and relationships
-    upper_current = (x_values[0], y_values[0] + epsilon)
-    upper_left_support = (x_values[0], y_values[0] + epsilon)
-    upper_right_support = (x_values[1], y_values[1] + epsilon)
-    upper_successors = {(x_values[0], y_values[0] + epsilon): (x_values[1], y_values[1] + epsilon)}
-    upper_predecessors = {(x_values[1], y_values[1] + epsilon): (x_values[0], y_values[0] + epsilon)}
-
-    # Lower tunnel boundary points and relationships
-    lower_current = (x_values[0], y_values[0] - epsilon)
-    lower_left_support = (x_values[0], y_values[0] - epsilon)
-    lower_right_support = (x_values[1], y_values[1] - epsilon)
-    lower_successors = {(x_values[0], y_values[0] - epsilon): (x_values[1], y_values[1] - epsilon)}
-    lower_predecessors = {(x_values[1], y_values[1] - epsilon): (x_values[0], y_values[0] - epsilon)}
-
-    # Result storage
-    pivot_points = []
-    current_index = 2
-
-    # Maximum iterations to prevent infinite loops
-    max_iterations = len(y_values) * 2
-
-    while current_index < len(y_values):
-        # Update upper convex hull
-        upper_prev_point = (x_values[current_index - 1], y_values[current_index - 1] + epsilon)
-        upper_current_point = (x_values[current_index], y_values[current_index] + epsilon)
-
-        iteration_count = 0
-        while (upper_prev_point != upper_current) and calculate_angle(upper_current_point, upper_prev_point,
-                                                                      upper_predecessors[upper_prev_point],
-                                                                      '+') > np.pi:
-            upper_prev_point = upper_predecessors[upper_prev_point]
-            iteration_count += 1
-            if iteration_count > max_iterations:
-                break  # Prevent infinite loops
-
-        upper_successors[upper_prev_point] = upper_current_point
-        upper_predecessors[upper_current_point] = upper_prev_point
-
-        # Update lower convex hull
-        lower_prev_point = (x_values[current_index - 1], y_values[current_index - 1] - epsilon)
-        lower_current_point = (x_values[current_index], y_values[current_index] - epsilon)
-
-        iteration_count = 0
-        while (lower_prev_point != lower_current) and calculate_angle(lower_current_point, lower_prev_point,
-                                                                      lower_predecessors[lower_prev_point],
-                                                                      '-') > np.pi:
-            lower_prev_point = lower_predecessors[lower_prev_point]
-            iteration_count += 1
-            if iteration_count > max_iterations:
-                break  # Prevent infinite loops
-
-        lower_successors[lower_prev_point] = lower_current_point
-        lower_predecessors[lower_current_point] = lower_prev_point
-
-        # Check if upper and lower convex hulls intersect
-        if calculate_angle(upper_current_point, upper_left_support, lower_right_support, '+') < np.pi:
-            # Hulls intersect - create a new pivot point
-            intersection = find_intersection(upper_left_support, lower_right_support, upper_current, lower_current)
-            if intersection:
-                pivot_points.append(
-                    (intersection, upper_left_support, lower_right_support, upper_current, lower_current))
-
-                # Update hull points
-                lower_current = lower_right_support
-                upper_current = find_intersection(upper_left_support, lower_right_support,
-                                                  (x_values[current_index - 1], y_values[current_index - 1] + epsilon),
-                                                  upper_current_point)
-
-                if upper_current:
-                    upper_successors[upper_current] = upper_current_point
-                    upper_predecessors[upper_current_point] = upper_current
-                    upper_right_support = upper_current_point
-                    lower_right_support = lower_current_point
-                    upper_left_support = upper_current
-                    lower_left_support = lower_current
-
-                    iteration_count = 0
-                    while lower_left_support in lower_successors and calculate_angle(lower_left_support,
-                                                                                     upper_right_support,
-                                                                                     lower_successors[
-                                                                                         lower_left_support],
-                                                                                     '-') < np.pi:
-                        lower_left_support = lower_successors[lower_left_support]
-                        iteration_count += 1
-                        if iteration_count > max_iterations:
-                            break
-
-        elif calculate_angle(lower_current_point, lower_left_support, upper_right_support, '-') < np.pi:
-            # Hulls intersect - create a new pivot point
-            intersection = find_intersection(lower_left_support, upper_right_support, lower_current, upper_current)
-            if intersection:
-                pivot_points.append(
-                    (intersection, lower_left_support, upper_right_support, lower_current, upper_current))
-
-                # Update hull points
-                upper_current = upper_right_support
-                lower_current = find_intersection(lower_left_support, upper_right_support,
-                                                  (x_values[current_index - 1], y_values[current_index - 1] - epsilon),
-                                                  lower_current_point)
-
-                if lower_current:
-                    lower_successors[lower_current] = lower_current_point
-                    lower_predecessors[lower_current_point] = lower_current
-                    lower_right_support = lower_current_point
-                    upper_right_support = upper_current_point
-                    lower_left_support = lower_current
-                    upper_left_support = upper_current
-
-                    iteration_count = 0
-                    while upper_left_support in upper_successors and calculate_angle(upper_left_support,
-                                                                                     lower_right_support,
-                                                                                     upper_successors[
-                                                                                         upper_left_support],
-                                                                                     '+') < np.pi:
-                        upper_left_support = upper_successors[upper_left_support]
-                        iteration_count += 1
-                        if iteration_count > max_iterations:
-                            break
+        if calculate_angle(p_i_plus, l_plus, r_minus, '+') < np.pi:
+            q.append((find_intersection(l_plus, r_minus, p_plus, p_minus), l_plus, r_minus, p_plus, p_minus))
+            p_minus = r_minus
+            p_plus = find_intersection(l_plus, r_minus, (x[i - 1], y[i - 1] + w), p_i_plus)
+            s_plus[p_plus] = p_i_plus
+            t_plus[p_i_plus] = p_plus
+            r_plus = p_i_plus
+            r_minus = p_i_minus
+            l_plus = p_plus
+            l_minus = p_minus
+            while l_minus in s_minus and calculate_angle(l_minus, r_plus, s_minus[l_minus], '-') < np.pi:
+                l_minus = s_minus[l_minus]
+        elif calculate_angle(p_i_minus, l_minus, r_plus, '-') < np.pi:
+            q.append((find_intersection(l_minus, r_plus, p_minus, p_plus), l_minus, r_plus, p_minus, p_plus))
+            p_plus = r_plus
+            p_minus = find_intersection(l_minus, r_plus, (x[i - 1], y[i - 1] - w), p_i_minus)
+            s_minus[p_minus] = p_i_minus
+            t_minus[p_i_minus] = p_minus
+            r_minus = p_i_minus
+            r_plus = p_i_plus
+            l_minus = p_minus
+            l_plus = p_plus
+            while l_plus in s_plus and calculate_angle(l_plus, r_minus, s_plus[l_plus], '+') < np.pi:
+                l_plus = s_plus[l_plus]
         else:
-            # Update the two separating and supporting lines
-            if calculate_angle(upper_current_point, lower_left_support, upper_right_support, '+') < np.pi:
-                upper_right_support = upper_current_point
+            if calculate_angle(p_i_plus, l_minus, r_plus, '+') < np.pi:
+                r_plus = p_i_plus
+                while l_minus in s_minus and calculate_angle(p_i_plus, l_minus, s_minus[l_minus], '+') < np.pi:
+                    l_minus = s_minus[l_minus]
 
-                iteration_count = 0
-                while calculate_angle(upper_current_point, lower_left_support, lower_successors[lower_left_support],
-                                      '+') < np.pi:
-                    lower_left_support = lower_successors[lower_left_support]
-                    iteration_count += 1
-                    if iteration_count > max_iterations:
-                        break
+            if calculate_angle(p_i_minus, l_plus, r_minus, '-') < np.pi:
+                r_minus = p_i_minus
+                while l_plus in s_plus and calculate_angle(p_i_minus, l_plus, s_plus[l_plus], '-') < np.pi:
+                    l_plus = s_plus[l_plus]
+        i += 1
 
-            if calculate_angle(lower_current_point, upper_left_support, lower_right_support, '-') < np.pi:
-                lower_right_support = lower_current_point
+    a = find_intersection(l_plus, r_minus, p_plus, p_minus)
+    b = find_intersection(l_minus, r_plus, p_minus, p_plus)
+    if a is None or b is None:
+        return np.array([])
+    p = ((a[0] + b[0]) / 2, (a[1] + b[1]) / 2)
+    q.append((p, r_minus, r_plus, p_minus, p_plus))
 
-                iteration_count = 0
-                while calculate_angle(lower_current_point, upper_left_support, upper_successors[upper_left_support],
-                                      '-') < np.pi:
-                    upper_left_support = upper_successors[upper_left_support]
-                    iteration_count += 1
-                    if iteration_count > max_iterations:
-                        break
+    end_a = find_intersection(p, r_plus, p_i_minus, p_i_plus)
+    end_b = find_intersection(p, r_minus, p_i_minus, p_i_plus)
+    if end_a is not None and end_b is not None:
+        end = ((end_a[0] + end_b[0]) / 2, (end_a[1] + end_b[1]) / 2)
+        q.append((end, (None, None), (None, None), p_i_minus, p_i_plus))
 
-        current_index += 1
-
-    # Add final pivot points
-    intersection1 = find_intersection(upper_left_support, lower_right_support, upper_current, lower_current)
-    intersection2 = find_intersection(lower_left_support, upper_right_support, lower_current, upper_current)
-
-    if intersection1 and intersection2:
-        midpoint = ((intersection1[0] + intersection2[0]) / 2, (intersection1[1] + intersection2[1]) / 2)
-        pivot_points.append((midpoint, lower_right_support, upper_right_support, lower_current, upper_current))
-
-        final_point1 = find_intersection(midpoint, upper_right_support, lower_current_point, upper_current_point)
-        final_point2 = find_intersection(midpoint, lower_right_support, lower_current_point, upper_current_point)
-
-        if final_point1 and final_point2:
-            final_midpoint = ((final_point1[0] + final_point2[0]) / 2, (final_point1[1] + final_point2[1]) / 2)
-            pivot_points.append((final_midpoint, (None, None), (None, None), lower_current_point, upper_current_point))
-
-    optimal_pc_linear_fx = np.array([point[0] for point in pivot_points])
+    optimal_pc_linear_fx = np.array([o[0] for o in q])
     optimal_num_pieces = len(optimal_pc_linear_fx) - 1
     given_num_pieces = len(pc_linear_fx) - 1
     return optimal_pc_linear_fx, optimal_num_pieces, given_num_pieces
+
 def reconstruct_piecewise_function(pc_linear_fx):
-    """
-    Reconstructs a piecewise linear function from points.
-
-    Args:
-        pivot_points (np.array): Array of (x,y) pivot points defining the piecewise linear function
-
-    Returns:
-        np.array: y-values of the reconstructed function
-    """
     pivot_points = sorted(pc_linear_fx, key=lambda p: p[0])
     y_values = []
     for i in range(len(pivot_points) - 1):
         start_point = pivot_points[i]
         end_point = pivot_points[i + 1]
-
-        # Handle potential division by zero
         if end_point[0] == start_point[0]:
             continue
-
-        # Calculate line equation: y = mx + b
         slope = (start_point[1] - end_point[1]) / (start_point[0] - end_point[0])
         intercept = start_point[1] - start_point[0] * slope
-
-        # Generate points along the line segment
         for x in range(int(start_point[0]), int(end_point[0])):
             y_values.append(x * slope + intercept)
-
+    y_values.append(pivot_points[-1][1])
     return np.array(y_values)
 def plot_piecewise_linear_approximation(pc_linear_fx, optimal_pc_linear_fx, epsilon):
     """
