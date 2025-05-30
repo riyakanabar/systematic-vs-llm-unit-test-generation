0a2afe14dc880a2f0f880024901c76298868ddac
Riya Kanabar <riyamkanabar1608@gmail.com>
2025-05-29 22:50:24 -0700

    add a test case

--- pc_linear/sample_run.py
+++ pc_linear/sample_run.py
@@ -1,8 +1,8 @@
 from pc_linear_apx import approximate_pc_linear_fx, plot_piecewise_linear_approximation
 from test_cases import test_cases
 
-pc_linear_fx = test_cases[6][0]
-epsilon = test_cases[6][1]
+pc_linear_fx = test_cases[9][0]
+epsilon = test_cases[9][1]
 
 optimal_pc_linear_fx, optimal_num_pieces, given_num_pieces = approximate_pc_linear_fx(pc_linear_fx, epsilon)
 print(optimal_pc_linear_fx)
